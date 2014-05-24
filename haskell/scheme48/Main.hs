963c7148000c1daabad19e2a88593386b12b0cbe
Pascal Hartig <passy@twitter.com>
2014-05-24 16:46:20 +0100

    Section 3: Return values

--- haskell/scheme48/Main.hs
+++ haskell/scheme48/Main.hs
@@ -1,6 +1,15 @@
 import Prelude
+import Control.Monad (liftM)
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+    deriving Show
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -9,9 +18,34 @@ spaces :: Parser ()
 spaces = skipMany1 space
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
     Left err -> "No match: " ++ show err
-    Right _ -> "Found value"
+    Right val -> "Found value: " ++ show val
+
+parseString :: Parser LispVal
+parseString = do
+    _ <- char '"'
+    x <- many (noneOf "\"")
+    _ <- char '"'
+    return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+    first <- letter <|> symbol
+    rest <- many (letter <|> digit <|> symbol)
+    let atom = first : rest
+    return $ case atom of
+        "#t" -> Bool True
+        "#f" -> Bool False
+        _ -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+    <|> parseString
+    <|> parseNumber
 
 main :: IO ()
 main = do
