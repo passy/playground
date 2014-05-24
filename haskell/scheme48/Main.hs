1323445eb5b00fc810e08c7e0c76e950c97748e3
Pascal Hartig <passy@twitter.com>
2014-05-24 17:27:10 +0100

    Numbers and characters

--- haskell/scheme48/Main.hs
+++ haskell/scheme48/Main.hs
@@ -1,4 +1,5 @@
 import Prelude
+import Numeric
 import Control.Monad (liftM)
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
@@ -9,13 +10,30 @@ data LispVal = Atom String
              | Number Integer
              | String String
              | Bool Bool
+             | Character Char
     deriving Show
 
 symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+symbol = oneOf "!$%&|*+-/:<=>?@^_~"
 
 spaces :: Parser ()
 spaces = skipMany1 space
+
+oct2dig :: (Num a, Eq a) => String -> a
+oct2dig x = fst $ readOct x !! 0
+
+hex2dig :: (Num a, Eq a) => String -> a
+hex2dig x = fst $ readHex x !! 0
+
+bin2dig :: [Char] -> Integer
+bin2dig = bin2dig' 0
+    where
+        bin2dig' digint "" = digint
+        bin2dig' digint (x:xs) =
+            let
+                old = 2 * digint + (if x == '0' then 0 else 1)
+            in
+                bin2dig' old xs
 
 escapedChars :: Parser Char
 escapedChars = do
@@ -52,12 +70,56 @@ parseAtom = do
         _ -> Atom atom
 
 parseNumber :: Parser LispVal
-parseNumber = liftM (Number . read) $ many1 digit
+parseNumber = parseDigital1 <|> parseDigital2 <|> parseHex <|> parseOct <|> parseBin
+
+parseDigital1 :: Parser LispVal
+parseDigital1 = many1 digit >>= (return . Number . read)
+
+parseDigital2 :: Parser LispVal
+parseDigital2 = do
+    _ <- try $ string "#d"
+    x <- many1 digit
+    (return . Number . read) x
+
+parseHex :: Parser LispVal
+parseHex = do
+    _ <- try $ string "#x"
+    x <- many1 hexDigit
+    return $ Number (hex2dig x)
+
+parseOct :: Parser LispVal
+parseOct = do
+    _ <- try $ string "#o"
+    x <- many1 octDigit
+    return $ Number (oct2dig x)
+
+parseBin :: Parser LispVal
+parseBin = do
+    _ <- try $ string "#b"
+    x <- many1 (oneOf "10")
+    return $ Number (bin2dig x)
+
+parseCharacter :: Parser LispVal
+parseCharacter = do
+    _ <- try $ string "#\\"
+    value <- try (string "newline" <|> string "space")
+        <|> do { x <- anyChar; notFollowedBy alphaNum; return [x] }
+    return $ Character $ case value of
+        "space" -> ' '
+        "newline" -> '\n'
+        _ -> (value !! 0)
+
+parseBool :: Parser LispVal
+parseBool = do
+    _ <- char '#'
+    ((char 't' >> return (Bool True)) <|> (char 'f' >> return (Bool True)))
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
     <|> parseString
-    <|> parseNumber
+    <|> try parseNumber
+    <|> try parseBool
+    <|> try parseCharacter
 
 main :: IO ()
 main = do
