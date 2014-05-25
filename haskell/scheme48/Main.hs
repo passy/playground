a9cb22767392dff188b8a43adff8a8728a9a0417
Pascal Hartig <passy@twitter.com>
2014-05-25 15:15:20 +0100

    Add eqv implementation

--- haskell/scheme48/Main.hs
+++ haskell/scheme48/Main.hs
@@ -207,6 +207,24 @@ cons [x, DottedList xs xlast] = return $ DottedList (x : xs) xlast
 cons [x1, x2] = return $ DottedList [x1] x2
 cons badArgList = throwError $ NumArgs 2 badArgList
 
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [(Bool arg1), (Bool arg2)] = return $ Bool $ arg1 == arg2
+eqv [(Number arg1), (Number arg2)] = return $ Bool $ arg1 == arg2
+eqv [(String arg1), (String arg2)] = return $ Bool $ arg1 == arg2
+eqv [(Atom arg1), (Atom arg2)] = return $ Bool $ arg1 == arg2
+eqv [(DottedList xs x), (DottedList ys y)] =
+    eqv [List $ xs ++ [x], List $ ys ++ [y]]
+eqv [(List arg1), (List arg2)] =
+    return $ Bool $ (length arg1 == length arg2) && (all eqvPair $ zip arg1 arg2)
+    where
+        eqvPair (x1, x2) =
+            case eqv [x1, x2] of
+                Right (Bool val) -> val
+                Right (_) -> undefined
+                Left _ -> False -- Cannot be reached.
+eqv [_, _] = return $ Bool False
+eqv badArgList = throwError $ NumArgs 2 badArgList
+
 parseString :: Parser LispVal
 parseString = do
     _ <- char '"'
