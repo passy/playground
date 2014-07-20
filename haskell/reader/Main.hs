21cda47b6b215dc25b52f175f8157aacd80f4775
Pascal Hartig <passy@twitter.com>
2014-07-20 23:01:24 +0100

    Reader: new example

--- haskell/reader/Main.hs
+++ haskell/reader/Main.hs
@@ -13,34 +13,31 @@ ask = Reader id
 -- Example Code
 
 data MyContext = MyContext {
-    foo :: String,
-    bar :: Int
-} deriving (Show)
+    name :: String,
+    age :: Int
+}
 
-computation :: Reader MyContext (Maybe String)
-computation = do
-    n <- asks bar
-    x <- asks foo
-    if n > 0
-        then return (Just x)
-        else return Nothing
+bartender :: Reader MyContext (Maybe String)
+bartender = do
+    n <- asks name
+    a <- asks age
+    return $ if a >= 18
+        then (Just n)
+        else Nothing
 
-ex1 :: Maybe String
-ex1 = runReader computation $ MyContext "hello" 1
-
-ex2 :: Maybe String
-ex2 = runReader computation $ MyContext "haskell" 0
-
-ex3 :: String
-ex3 = runReader computation2 "Haskell"
+pub :: IO ()
+pub =
+    putStrLn $ case computation of
+        (Just n) -> "Have a lager, " ++ n
+        Nothing -> "No drinks for you"
     where
-        computation2 :: Reader String String
-        computation2 = do
-            name <- ask
-            return $ "Hello, " ++ name
+        computation :: Maybe String
+        computation = runReader bartender $ MyContext "@phuunet" 16
+
+-- DI Example
+data DIContext = DIContext {
+    prettyPrint :: String -> Int -> String
+}
 
 main :: IO ()
-main = do
-    print ex1
-    print ex2
-    print ex3
+main = pub
