c4d362c40a40f46b7008c8ec634d8d5170d8f6d9
Pascal Hartig <passy@twitter.com>
2014-07-27 14:29:22 +0100

    Utter bollocks

--- javascript/reader/main.js
+++ javascript/reader/main.js
@@ -1,14 +1,35 @@
-var DOMWriter = function (id, content, config) {
-    config.render(content);
-
-    return "Hullo";
-}.reader();
-
-var main = function () {
-    var config = {
-        renderer: function (el, c) { el.innerHTML = c; }
-    };
-    DOMWriter("#main", "Hello, World").map(function (r) { console.log(r); }).run(config)
+var Reader = function (fn) {
+    this.f = fn;
 };
 
-main();
+Reader.prototype.run = function (ctx) {
+    return this.f(ctx);
+};
+
+Reader.prototype.map = function (fn) {
+    return new Reader(function (ctx) {
+        return fn(this.run(ctx));
+    }.bind(this));
+};
+
+Reader.prototype.flatMap = function (fn) {
+    return new Reader(function (ctx) {
+        return fn(this.run(ctx)).run(ctx);
+    }.bind(this));
+};
+
+var simple = new Reader(function () {
+    return this.ask();
+});
+
+var computation = new Reader(function (name) {
+    this.ask().bind(function (greeting) {
+        return this.unit(greeting + ", " + name);
+    });
+});
+
+var example = function () {
+    console.log(computation("Tom").runReader("Hello"));
+};
+
+example();
