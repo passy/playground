2c64bc84ac35773b97758fa2802a7c3eb7e4e945
Pascal Hartig <passy@twitter.com>
2015-01-14 08:55:06 -0500

    Half an applicative

--- haskell/list/List.hs
+++ haskell/list/List.hs
@@ -1,9 +1,20 @@
+{-# Language KindSignatures #-}
+
 data List a = End | Cons a (List a)
     deriving (Show)
 
 instance Functor List where
     fmap _ End         = End
     fmap f (Cons x xs) = Cons (f x) (fmap f xs)
+
+
+class Functor f => Applicative (f :: * -> *) where
+    pure :: a -> f a
+    (<*>) :: f (a -> b) -> f a -> f b
+
+instance Applicative List where
+    pure a = Cons a End
+    (<*>) = undefined
 
 main :: IO ()
 main = do
