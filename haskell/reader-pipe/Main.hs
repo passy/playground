e39fde970b0bebf3ffe2ac882b442b3d938378e2
Pascal Hartig <phartig@twitter.com>
2015-02-07 16:12:26 +0000

    reader-pipe: Use CT.decode

--- haskell/reader-pipe/Main.hs
+++ haskell/reader-pipe/Main.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE NoImplicitPrelude, OverloadedStrings #-}
 
-
 import Prelude hiding (mapM_)
 
 import Data.Foldable (mapM_)
@@ -8,10 +7,10 @@ import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Trans.Resource (runResourceT)
 
 import qualified Data.Text as T
-import qualified Data.Text.Encoding as TE
 import qualified Data.Conduit as C
 import qualified Data.Conduit.List as CL
 import qualified Data.Conduit.Binary as CB
+import qualified Data.Conduit.Text as CT
 
 
 mapWords :: MonadIO m => C.Conduit T.Text m (T.Text, Int)
@@ -19,19 +18,18 @@ mapWords = C.await >>= mapM_ (\w -> C.yield (w, 1)) >> mapWords
 
 
 reduceWords :: MonadIO m => C.Conduit (T.Text, Int) m [(T.Text, Int)]
-reduceWords = do
-    yo <- CL.fold (\a b -> a) []
-    C.yield yo >> reduceWords
+reduceWords = undefined
 
 main :: IO ()
 main = do
-    result <- runResourceT $ CB.sourceFile "shakespeare.txt" C.$$
+    res <- runResourceT $
+        CB.sourceFile "shakespeare.txt" C.$$
         CB.lines C.$=
-        CL.map (T.strip . TE.decodeUtf8) C.$=
+        CT.decode CT.utf8 C.$=
         CL.filter (not . T.null) C.$=
         CL.mapFoldable T.words C.$=
         mapWords C.$=
-        reduceWords C.$=
+        -- reduceWords C.$=
         CL.take 5
 
-    print result
+    print res
