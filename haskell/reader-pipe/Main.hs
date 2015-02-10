86a1a232479bb6c94356414a0d90c6ef659cb25d
Pascal Hartig <phartig@twitter.com>
2015-02-10 21:09:11 +0000

    Add a so far useless scoring

--- haskell/reader-pipe/Main.hs
+++ haskell/reader-pipe/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NoImplicitPrelude, OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude, OverloadedStrings, RankNTypes #-}
 
 import Prelude hiding (mapM_)
 
@@ -17,9 +17,17 @@ import qualified Data.Conduit.Text as CT
 mapWords :: MonadIO m => C.Conduit T.Text m (T.Text, Int)
 mapWords = C.await >>= mapM_ (\w -> C.yield (w, 1)) >> mapWords
 
-
 reduceScore :: MonadIO m => C.Sink (T.Text, Int) m Int
 reduceScore = CL.fold (\a b -> a + snd b) 0
+
+(-:) :: forall a b. a -> b -> (a, b)
+(-:) = (,)
+
+scoring :: [(T.Text, Int)]
+scoring = [ "winters" -: 10
+          , "pilgrimage" -: 8
+          , "rug-headed" -: 5
+          ]
 
 main :: IO ()
 main = do
