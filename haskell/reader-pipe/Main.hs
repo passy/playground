478a0bb62860083466e10fee61c08b818df34bfa
Pascal Hartig <phartig@twitter.com>
2015-02-11 22:08:42 +0000

    Add a basic scoring function

--- haskell/reader-pipe/Main.hs
+++ haskell/reader-pipe/Main.hs
@@ -2,7 +2,7 @@
 
 import Prelude hiding (mapM_)
 
-import Data.Foldable (mapM_)
+import Data.Maybe (fromMaybe)
 import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Trans.Resource (runResourceT)
 
@@ -12,10 +12,21 @@ import qualified Data.Conduit.List as CL
 import qualified Data.Conduit.Binary as CB
 import qualified Data.Conduit.Text as CT
 
+type ScoreMap = [(T.Text, Int)]
+
 
 -- TODO: Let this take some scoring read from a Reader into account
-mapWords :: MonadIO m => C.Conduit T.Text m (T.Text, Int)
-mapWords = C.await >>= mapM_ (\w -> C.yield (w, 1)) >> mapWords
+mapWordsScore :: MonadIO m => ScoreMap -> C.Conduit T.Text m (T.Text, Int)
+-- TODO: Make it work, make it pretty, make it ... don't care, but no nested
+-- pattern matches ffs.
+mapWordsScore scores = do
+    res <- C.await
+    case res of
+         (Just word) -> do
+             let s = lookup word scores
+             C.yield (word, fromMaybe 0 s)
+             mapWordsScore scores
+         _ -> return ()
 
 reduceScore :: MonadIO m => C.Sink (T.Text, Int) m Int
 reduceScore = CL.fold (\a b -> a + snd b) 0
@@ -23,7 +34,7 @@ reduceScore = CL.fold (\a b -> a + snd b) 0
 (-:) :: forall a b. a -> b -> (a, b)
 (-:) = (,)
 
-scoring :: [(T.Text, Int)]
+scoring :: ScoreMap
 scoring = [ "winters" -: 10
           , "pilgrimage" -: 8
           , "rug-headed" -: 5
@@ -36,8 +47,10 @@ main = do
         CB.lines C.$=
         CT.decode CT.utf8 C.$=
         CL.filter (not . T.null) C.$=
+        -- Useful for debugging, limit to n lines.
+        -- CL.isolate 16 C.$=
         CL.mapFoldable T.words C.$=
-        mapWords C.$=
+        mapWordsScore scoring C.$=
         reduceScore
 
     print res
