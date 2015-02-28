45634bed0f7ee7bb365c1a9b05edfcee320bf439
Pascal Hartig <phartig@twitter.com>
2015-02-28 01:15:20 +0000

    Add trim function

--- haskell/lrucache/Data/Cache/LRU.hs
+++ haskell/lrucache/Data/Cache/LRU.hs
@@ -20,3 +20,16 @@ empty capacity
         , T.tick     = 0
         , T.queue    = HashPSQ.empty
         }
+
+-- TODO: Should that constraint be part of Cache itself somehow?
+trim :: (Hashable k, Ord k) => T.Cache k v -> T.Cache k v
+trim c
+    | T.tick c == maxBound =
+        -- Taking the easy way out, just reset everything
+        empty (T.capacity c)
+        -- TODO: Use lenses here
+    | T.size c > T.capacity c = c
+        { T.size = T.size c - 1
+        , T.queue = HashPSQ.deleteMin (T.queue c)
+        }
+    | otherwise = c
