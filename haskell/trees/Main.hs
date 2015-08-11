02c81b27d9c3a368faae48a4c96fb45d83ecdf30
Pascal Hartig <phartig@twitter.com>
2015-08-11 21:34:03 +0100

    Finish imperative implementation of BFS

--- haskell/trees/Main.hs
+++ haskell/trees/Main.hs
@@ -1,41 +1,68 @@
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
+import GHC.Generics (Generic)
 
 import Prelude
 import Data.Sequence (ViewL(..))
 import Data.Foldable (toList)
+import Data.Monoid
 import qualified Data.Sequence as Seq
-import qualified Data.Set as Set
+import qualified Data.HashSet as Set
 import Control.Monad.State
 import Control.Monad.Loops
+import Data.Hashable
 
 data Tree a = Node a (Tree a) (Tree a) | Empty
-  deriving (Show, Functor)
+  deriving (Show, Functor, Eq, Generic, Hashable)
+
+adjacentNodes :: Tree a -> [Tree a]
+adjacentNodes Empty = []
+adjacentNodes (Node _ Empty Empty) = []
+adjacentNodes (Node _ left Empty) = [left]
+adjacentNodes (Node _ Empty right) = [right]
+adjacentNodes (Node _ left right) = [left, right]
 
 traverseDF :: Tree a -> [a]
 traverseDF Empty = []
-traverseDF (Node v l r) =
-  [v] ++ traverseDF l ++ traverseDF r
+traverseDF (Node v l r) = [v] ++ traverseDF l ++ traverseDF r
 
--- Imperative first.
-traverseBF :: Tree a -> [a]
+-- Imperative first. (And I know that I'm vastly overcomplicating this, but
+-- this has been an incredible learning experience.)
+traverseBF :: (Hashable a, Eq a) => Tree a -> [a]
 traverseBF Empty = []
-traverseBF (Node v l r) = (flip execState) (Seq.singleton v) $ do
-  -- TODO: Use this, needs to be part of the state, though.
-  let visited = Set.empty :: Set.Set a
-
+traverseBF node = flip evalState (Seq.singleton node, Set.empty) $
   -- While the queue is not empty
-  whileM (not . Seq.null <$> get) $ do
-    -- Get the queue
-    q <- get
-    -- Get its head and tail
-    let (n :< ns) = Seq.viewl q
-    -- XXX: Set the tail as new queue
-    -- Should have the adjacent nodes.
-    put ns
-    return $ Seq.singleton v
+  whileM (not . Seq.null . fst <$> get) $ do
+    -- Get the state
+    (queue, visited) <- get
+    -- Pop the first item off the queue
+    let (n@(Node v _ _) :< ns) = Seq.viewl queue
+    -- Get all adjacent nodes that haven't been marked before
+    let adj = Seq.fromList [ n' | n' <- adjacentNodes n, not $ Set.member n' visited ]
+    -- Update the state
+    put (ns <> adj, n `Set.insert` visited)
+    -- Push this into the result
+    return v
+
+createTree :: Tree Char
+createTree = Node 'A'
+                (Node 'B'
+                    (Node 'C' Empty Empty)
+                    (Node 'D' Empty Empty)
+                )
+                (Node 'E'
+                    (Node 'F' Empty Empty)
+                    (Node 'G' Empty (Node 'H'
+                        (Node 'I' Empty Empty)
+                        Empty
+                    ))
+                )
 
 main :: IO ()
 main = do
-  let t = Node "A" (Node "B" (Node "C" Empty Empty) Empty) (Node "D" Empty (Node "E" (Node "F" Empty Empty) Empty))
-  -- print $ traverseDF t
-  print $ traverseBF t
+  putStrLn "DFS:"
+  print $ traverseDF createTree
+  putStrLn "BFS:"
+  print $ traverseBF createTree
