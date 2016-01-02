1f8e5d95379d29f3bc85cec09bf7c46a307af0c4
Pascal Hartig <phartig@rdrei.net>
2016-01-02 22:58:58 +0100

    horribly mutable (aka in-place) quicksort

--- javascript/quickselect/main.js
+++ javascript/quickselect/main.js
@@ -26,8 +26,28 @@ const partition = (arr, lo, hi) => {
   return i;
 };
 
-const arr = [5, 3, 10, 7, 9, 2, 1];
-const p = partition(arr, 0, arr.length - 1);
+const quicksort = (arr, lo, hi) => {
+  if (lo < hi) {
+    const p = partition(arr, lo, hi);
+    quicksort(arr, lo, p);
+    quicksort(arr, p + 1, hi);
+  }
+};
 
-console.log('p =', p);
+const rselect = (arr, order) => {
+  const go = (lo, hi, o) => {
+    if (hi - lo === 0) return arr[lo];
+    const p = partition(arr, lo, hi);
+    if (p === o) return arr[o];
+    if (p > o) return go(lo, p - 1, o);
+    if (p < o) return go(p + 1, hi, p + o);
+  };
+
+  return go(0, arr.length - 1, order);
+};
+
+const arr = [5, 3, 10, 7, 9, 2, 1];
+const o = quicksort(arr, 0, arr.length - 1);
+
+console.log('o =', o);
 console.log('arr =', arr);
