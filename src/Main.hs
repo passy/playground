0bfba8694563df5d10f8f097a270bdacc3d0cbe1
Pascal Hartig <phartig@rdrei.net>
2016-05-29 03:01:25 -0700

    This is what I think should be working but isn't

--- src/Main.hs
+++ src/Main.hs
@@ -2,17 +2,23 @@
 
 module Main where
 
-import qualified Text.Megaparsec as M
+import qualified System.Environment   as Env
+import qualified Text.Megaparsec      as M
 import qualified Text.Megaparsec.Text as M
-import qualified System.Environment as Env
 
 type Tag = (String, String)
 
 tagP :: M.Parser Tag
-tagP = M.char '#' *> ((,) <$> M.someTill M.printChar (M.char ':') <*> M.someTill M.printChar M.eol)
+tagP = M.char '#' *> ((,) <$> M.someTill M.printChar (M.char ':') <*> M.someTill M.printChar M.eol) M.<?> "Tag starting with #"
 
-parser :: M.Parser [Tag]
-parser = M.many tagP
+urlP :: M.Parser String
+urlP = M.someTill M.printChar M.eol M.<?> "Some URL"
+
+data Segment = Tags [Tag] | URL String
+  deriving (Eq, Show)
+
+parser :: M.Parser Segment
+parser = (Tags <$> M.many tagP) M.<|> (URL <$> urlP)
 
 main :: IO ()
 main = do
