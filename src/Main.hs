24548c474145e88f3dcbb9a6fb8134ff2ddb6d0b
Pascal Hartig <phartig@rdrei.net>
2016-05-29 02:54:29 -0700

    Simple sample parser

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Control.Monad (void)
 import qualified Text.Megaparsec as M
+import qualified Text.Megaparsec.Text as M
+import qualified System.Environment as Env
+
+type Tag = (String, String)
+
+tagP :: M.Parser Tag
+tagP = do
+  void $ M.char '#'
+  key <- M.someTill M.printChar (M.char ':')
+  val <- M.someTill M.printChar M.eol
+  return (key, val)
+
+parser :: M.Parser [Tag]
+parser = M.many tagP
 
 main :: IO ()
-main = putStrLn "hello"
+main = do
+  fname <- head <$> Env.getArgs
+  res <- M.parseFromFile (parser <* M.eof) fname
+  print res
