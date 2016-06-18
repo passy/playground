5b3878a2d57d5a2e823f9b59fd5386b913128aba
Pascal Hartig <phartig@rdrei.net>
2016-06-18 13:15:09 +0100

    Update for Megaparsec 5

--- haskell/parsing/src/Main.hs
+++ haskell/parsing/src/Main.hs
@@ -2,13 +2,15 @@
 
 module Main where
 
-import qualified Data.Text            as T
-import qualified Data.Text.IO         as TIO
 import qualified System.Environment   as Env
 import qualified Text.Megaparsec      as M
 import qualified Text.Megaparsec.Text as M
+import qualified Data.Text.IO         as TIO
 
 type Tag = (String, String)
+
+data Segment = Tags [Tag] | URL String
+  deriving (Eq, Show)
 
 tagP :: M.Parser Tag
 tagP = M.char '#' *> ((,) <$> M.someTill M.printChar (M.char ':') <*> M.someTill M.printChar M.eol) M.<?> "Tag starting with #"
@@ -16,17 +18,13 @@ tagP = M.char '#' *> ((,) <$> M.someTill M.printChar (M.char ':') <*> M.someTill
 urlP :: M.Parser String
 urlP = M.someTill M.printChar M.eol M.<?> "Some URL"
 
-data Segment = Tags [Tag] | URL String
-  deriving (Eq, Show)
-
 parser :: M.Parser Segment
 parser = (Tags <$> M.many tagP) M.<|> (URL <$> urlP)
-
-parseFromFile :: M.Parsec e T.Text a -> FilePath -> IO (Either (M.ParseError (M.Token T.Text) e) a)
-parseFromFile p fp = M.parse p fp <$> TIO.readFile fp
 
 main :: IO ()
 main = do
   fname <- head <$> Env.getArgs
-  res <- parseFromFile (parser <* M.eof) fname
-  print res
+  res <- M.parse (parser <* M.eof) mempty <$> TIO.readFile fname
+  case res of
+    Left err -> putStrLn $ M.parseErrorPretty err
+    Right res' -> print res'
