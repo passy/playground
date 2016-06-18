c41d3ccff9de061c7de0fe7f8c0495aeb6860e35
Pascal Hartig <phartig@rdrei.net>
2016-06-18 13:52:49 +0100

    Apply cocreature's suggestions

--- haskell/parsing/src/Main.hs
+++ haskell/parsing/src/Main.hs
@@ -2,10 +2,11 @@
 
 module Main where
 
+import           Control.Applicative  (liftA2)
+import qualified Data.Text.IO         as TIO
 import qualified System.Environment   as Env
 import qualified Text.Megaparsec      as M
 import qualified Text.Megaparsec.Text as M
-import qualified Data.Text.IO         as TIO
 
 type Tag = (String, String)
 
@@ -18,13 +19,13 @@ tagP = M.char '#' *> ((,) <$> M.someTill M.printChar (M.char ':') <*> M.someTill
 urlP :: M.Parser String
 urlP = M.someTill M.printChar M.eol M.<?> "Some URL"
 
-parser :: M.Parser Segment
-parser = (Tags <$> M.many tagP) M.<|> (URL <$> urlP)
+parser :: M.Parser ([Tag], String)
+parser = liftA2 (,) (M.many tagP) urlP
 
 main :: IO ()
 main = do
   fname <- head <$> Env.getArgs
-  res <- M.parse (parser <* M.eof) mempty <$> TIO.readFile fname
+  res <- M.parse (M.many parser <* M.eof) mempty <$> TIO.readFile fname
   case res of
     Left err -> putStrLn $ M.parseErrorPretty err
     Right res' -> print res'
