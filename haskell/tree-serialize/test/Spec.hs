893b6c0277dd9c6778b8a3abca774926d4518195
Pascal Hartig <phartig@rdrei.net>
2018-02-04 16:47:11 +0000

    Test setup

--- haskell/tree-serialize/test/Spec.hs
+++ haskell/tree-serialize/test/Spec.hs
@@ -1,10 +1,17 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 import           Hedgehog
+import Control.Monad (unless)
+import System.Exit (exitFailure)
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 
 
 tests :: IO Bool
 tests =
-  checkParallel $$(discover)
+  checkParallel $$discover
+
+main :: IO ()
+main = do
+  result <- tests
+  unless result exitFailure
